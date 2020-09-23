ffde6bed890d484ec5a561da367d0cab4bdf3304
Zhiru Cao <1074160@ucn.dk>
2020-09-23 11:19:45 +0200

    test

---
+++ GeneratorServiceTest/GeneratorServiceTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeneratorServiceTest {
+    [TestClass]
+    public class GeneratorServiceTest {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
---
+++ GeneratorServiceTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GeneratorServiceTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GeneratorServiceTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3449fde3-3d92-4497-b4d4-40a4825928d2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
