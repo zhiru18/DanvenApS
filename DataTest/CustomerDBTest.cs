a702af11fba9b81ca20d14783af9ba323ecf437c
Zhiru Cao <1074160@ucn.dk>
2020-09-29 09:36:01 +0200

    productdb and customerdb tests

---
+++ DataTest/CustomerDBTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfDanvenRepairedGenerator.DatabaseAccessLayer;
+using WcfDanvenRepairedGenerator.ModelLayer;
+
+namespace DataTest {
+    [TestClass]
+    public class CustomerDBTest {
+        CustomerDB customerDB;
+       
+        [TestMethod]
+        public void GetCustomerByIdTest() {
+            // Arrange
+            customerDB = new CustomerDB();
+            Customer customer = null, customer1 = null;
+            customer = customerDB.GetCustomerByTelephone("12121212");
+
+
+            //Act
+            customer1 = customerDB.GetCustomerById(customer.Id);
+
+            //Assert
+            Assert.AreEqual(customer.CompanyName, customer1.CompanyName);
+        }
+
+        [TestMethod]
+        public void UpdateGeneratorTest() {
+            // Arrange
+            customerDB = new CustomerDB();
+            Customer customer = customerDB.GetCustomerById(2);
+            //Act
+            customer.CompanyName = "Update Company";
+            customerDB.UpdateCustomer(customer);
+            var customer1 = customerDB.GetCustomerById(2);
+
+            //Assert
+            Assert.AreEqual(customer1.CompanyName, "Update Company");
+
+            //Cleanup
+            customer1.CompanyName = "Test Company";
+            customerDB.UpdateCustomer(customer1);
+        }
+
+        [TestMethod]
+        public void GetCustomerByTelephoneTest() {
+            // Arrange
+            customerDB = new CustomerDB();
+            Customer customer= null, customer1 = null;
+            customer = customerDB.GetCustomerById(2);
+
+
+            //Act
+            customer1 = customerDB.GetCustomerByTelephone(customer.Telephone);
+
+            //Assert
+            Assert.AreEqual(customer.CompanyName, customer1.CompanyName);
+        }     
+    }
+}
---
+++ DataTest/ProductDBTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfDanvenRepairedGenerator.DatabaseAccessLayer;
+using WcfDanvenRepairedGenerator.ModelLayer;
+
+namespace DataTest {
+    [TestClass]
+    public class ProductDBTest {
+        ProductDB productDB;
+        [TestMethod]
+        public void GetProductByIdTest() {
+            // Arrange
+            productDB = new ProductDB();
+            Product product = null, product1 = null;
+            product = productDB.GetProductByTypeInvoice("Test prod", "test inv");
+
+            //Act
+            product1 = productDB.GetProductById(2);
+
+            //Assert
+            Assert.AreEqual(product.InvoiceNumber, product1.InvoiceNumber);
+        }
+
+        [TestMethod]
+        public void UpdateGeneratorTest() {
+            // Arrange
+            productDB = new ProductDB();
+            Product product = productDB.GetProductById(2);
+            //Act
+            product.ProductType = "Update prod";
+            productDB.UpdateProduct(product);
+            var product1 = productDB.GetProductById(2);
+
+            //Assert
+            Assert.AreEqual(product1.ProductType, "Update prod");
+
+            //Cleanup
+            product1.ProductType = "Test prod";
+            productDB.UpdateProduct(product1);
+        }
+
+        [TestMethod]
+        public void GetProductByTypeInvoiceTest() {
+            // Arrange
+            productDB = new ProductDB();
+            Product product = null, product1 = null;
+            product = productDB.GetProductById(2);
+
+            //Act
+            product1 = productDB.GetProductByTypeInvoice(product.ProductType, product.InvoiceNumber);
+
+            //Assert
+            Assert.AreEqual(product1.ProductSerialNumber, product.ProductSerialNumber);
+        }
+    }
+}
