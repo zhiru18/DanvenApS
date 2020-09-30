669d4b23b5815be6fee991a8babb863e143be004
Zhiru Cao <1074160@ucn.dk>
2020-09-30 08:43:27 +0200

    desktop service access test

---
+++ DesktopTest/GeneratorServiceAccessTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using DesktopDanvenClient.ModelLayer;
+using DesktopDanvenClient.ServiceLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesktopTest {
+    [TestClass]
+    public class GeneratorServiceAccessTest {
+        GeneratorServiceAccess generatorServiceAccess;
+        [TestMethod]
+        public void GetAllTest() {
+            //Arrange
+            generatorServiceAccess = new GeneratorServiceAccess();
+
+            //Act
+            List<ClientGenerator> clientGenerators = generatorServiceAccess.GetAll();
+
+            //Assert
+            Assert.IsTrue(clientGenerators.Count > 0);
+        }
+
+        [TestMethod]
+        public void GetByIdTest() {
+            //Arrange
+            generatorServiceAccess = new GeneratorServiceAccess();
+            ClientGenerator clientGenerator = null, clientGenerator1 = null;
+            List<ClientGenerator> clientGenerators = generatorServiceAccess.GetAll();
+            if (clientGenerators.Count > 0) {
+                clientGenerator = clientGenerators[0];
+            }
+
+            //Act
+            clientGenerator1 = generatorServiceAccess.GetById(clientGenerator.Id);
+
+            //Assert
+            Assert.AreEqual(clientGenerator.TypeNumber, clientGenerator1.TypeNumber);
+        }
+
+        [TestMethod]
+        public void UpdateGeneratorTest() {
+            //Arrange
+            generatorServiceAccess = new GeneratorServiceAccess();
+            ClientGenerator clientGenerator = null, clientGenerator1 = null;
+            List<ClientGenerator> clientGenerators = generatorServiceAccess.GetAll();
+            if (clientGenerators.Count > 0) {
+                clientGenerator = clientGenerators[0];
+            }
+
+            //Act
+            string gamleSerialNumber = clientGenerator.SerialNumber;
+            clientGenerator.SerialNumber = "Update";
+            generatorServiceAccess.UpdateGenerator(clientGenerator);
+            clientGenerator1 = generatorServiceAccess.GetById(clientGenerator.Id);
+
+            //Assert
+            Assert.AreEqual(clientGenerator1.SerialNumber, "Update");
+
+            //Cleanup
+            clientGenerator1.SerialNumber = gamleSerialNumber;
+            generatorServiceAccess.UpdateGenerator(clientGenerator1);
+        }
+    } 
+}
---
+++ DesktopTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DesktopTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DesktopTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64a5404e-e7d4-453b-884a-1c81f2a86f54")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
