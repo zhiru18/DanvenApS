7f3376fc258604d5ea80d3343ad0478247c67bc3
Zhiru Cao <1074160@ucn.dk>
2020-10-01 08:34:10 +0200

    exception

--- WcfDanvenRepairedGenerator/ServiceAccessLayer/GeneratorService.cs
+++ WcfDanvenRepairedGenerator/ServiceAccessLayer/GeneratorService.cs
@@ -12,7 +12,10 @@ namespace WcfDanvenRepairedGenerator.ServiceAccessLayer {
     public class GeneratorService : IGeneratorService {
         GeneratorController generatorController = new GeneratorController();
         public Generator CreateGenerator(Generator generator) {
-            return generatorController.CreateGenerator(generator);
+            if (generator== null) {
+                throw new ArgumentNullException();
+            }else { return generatorController.CreateGenerator(generator); 
+            }         
         }
 
         public IEnumerable<Generator> GetAll() {
@@ -20,11 +23,17 @@ namespace WcfDanvenRepairedGenerator.ServiceAccessLayer {
         }
 
         public Generator GetById(int id) {
-            return generatorController.GetById(id);
+            if (id == 0) {
+                throw new ArgumentException();
+            } else { return generatorController.GetById(id); 
+            }           
         }
 
         public Generator UpdateGenerator(Generator generator) {
-            return generatorController.UpdateGenerator(generator);
+            if (generator == null) {
+                throw new ArgumentNullException();
+            } else {return generatorController.UpdateGenerator(generator); 
+            }           
         }
     }
 }
